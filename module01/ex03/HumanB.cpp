51a1a9b823a401a3e83204b9aac65d014235ae8c
Alibababs <alibaba@Alix>
2025-03-27 15:56:25 +0100

    * 01 - 03 finished

---
+++ module01/ex03/HumanB.cpp
@@ -0,0 +1,21 @@
+#include "Weapon.hpp"
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL)
+{
+}
+
+HumanB::~HumanB()
+{
+}
+
+void HumanB::attack(void) const
+{
+	if (_weapon)
+		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+	_weapon = &weapon;
+}
--- module01/ex03/Weapon.hpp
+++ module01/ex03/Weapon.hpp
@@ -8,7 +8,7 @@ class Weapon
     private:
         std::string _type;
     public:
-        Weapon();
+        Weapon(std::string type);
         ~Weapon();
 		const std::string &getType(void) const;
         void setType(std::string type);        
