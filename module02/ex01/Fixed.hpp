2227acc5c3ad7b83fd5189158f711f78b6b63506
Alix Bailly <pbailly@made-f0Br3s3.clusters.42paris.fr>
2025-04-02 17:11:21 +0200

    * Issou

---
+++ module02/ex01/Fixed.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Fixed
+{
+    private:
+        int _value;
+        const static int _fractionalBits = 8;
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed &copy);
+        Fixed &operator=(const Fixed &copy);
+
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+};
