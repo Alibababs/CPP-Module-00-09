06c0e800e3eed3d199927661fe759e714db48866
Alix Bailly <pbailly@paul-f3Ar6s2.clusters.42paris.fr>
2025-04-10 11:48:43 +0200

    * Useless

---
+++ module02/ex02/Fixed.cpp
@@ -0,0 +1,63 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed (int intValue)
+{
+    std::cout << "Int constructor called" << std::endl;
+    _value = intValue << _fractionalBits;
+}
+
+Fixed::Fixed(float floatValue)
+{
+    std::cout << "Float constructor called" << std::endl;
+    _value = roundf(floatValue * (1 << _fractionalBits));
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(const Fixed &copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &copy)
+		this->_value = copy.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	return (_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_value = raw;
+}
+
+int Fixed::toInt(void) const
+{
+	return (_value >> _fractionalBits);
+}
+
+float Fixed::toFloat(void) const
+{
+	return (float)_value / (1 << _fractionalBits);
+}
+
+std::ostream &operator<<( std::ostream &os, const Fixed &copy)
+{
+	os << copy.toFloat();
+	return (os);
+}
---
+++ module02/ex02/Fixed.hpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <iostream>
+
+class Fixed
+{
+  private:
+	int _value;
+	const static int _fractionalBits = 8;
+
+  public:
+	Fixed();
+	Fixed(int intValue);
+	Fixed(float floatValue);
+
+	~Fixed();
+	Fixed(const Fixed &copy);
+	Fixed &operator=(const Fixed &copy);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+    float toFloat( void ) const;
+	int toInt( void ) const;
+
+	
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &copy);
