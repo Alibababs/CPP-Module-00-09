440ad650a4b51e6f0c3203e45b38b9852451bd5f
Alix <alixbailly23@gmail.com>
2025-04-29 01:50:39 +0200

    * OUI A FAIRE

---
+++ module04/ex03/AMateria.cpp
@@ -0,0 +1,40 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria() : _type("notype")
+{
+    std::cout << "AMateria constructor called" << std::endl;
+}
+
+AMateria::AMateria(const AMateria &copy)
+{
+    std::cout << "Aninal copy constructor called" << std::endl;
+    *this = copy;
+}
+
+AMateria &AMateria::operator=(const AMateria &copy)
+{
+   std::cout << "AMateria copy assignment operator called" << std::endl;
+   if (this != &copy)
+        this->_type = copy._type;
+   return *this;
+}
+
+AMateria::~AMateria()
+{
+    std::cout << "AMateria destructor called" << std::endl;
+}
+
+AMateria::AMateria(std::string const & type) : _type(type)
+{
+	std::cout << "AMateria type constructor called" << std::endl;
+}
+
+const std::string &AMateria::getType() const
+{
+	return _type;
+}
+
+void AMateria::use(ICharacter& target)
+{
+	std::cout << "USE" << std::endl;
+}
---
+++ module04/ex03/AMateria.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class AMateria
+{
+	protected:
+		std::string _type;
+	public:
+		AMateria();
+		AMateria(const AMateria &copy);
+		AMateria &operator=(const AMateria &copy);
+		virtual ~AMateria();
+	
+		AMateria(std::string const & type);
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
