1109f93187346bc8c72f7115a3f28f74a11ec353
Alix Bailly <pbailly@f1r6s2.clusters.42paris.fr>
2025-06-16 16:37:11 +0200

    * OUi

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -9,12 +9,11 @@ int main(void)
         Point b(5.0f, 0.0f);
         Point c(2.5f, 5.0f);
 
-        Point p(2.5f, 2.0f);
+        Point p(2.5f, 0.0f);
 
         if (bsp(a, b, c, p))
                 std::cout << "True" << std::endl;
         else
                 std::cout << "False" << std::endl;
-
     return (0);
 }
