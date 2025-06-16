4a7fc6d840301aaba46a2123c1c730dabdf70c59
Alix Bailly <pbailly@f1r6s2.clusters.42paris.fr>
2025-06-16 16:47:48 +0200

    * MUCH BETTER

--- module02/ex03/bsp.cpp
+++ module02/ex03/bsp.cpp
@@ -1,21 +1,21 @@
 #include "Point.hpp"
 #include "Fixed.hpp"
 
-float sign(Point const &p1, Point const &p2, Point const &p3)
+Fixed sign(Point const &p1, Point const &p2, Point const &p3)
 {
-    return (p1.getX().toFloat() - p3.getX().toFloat()) * (p2.getY().toFloat() - p3.getY().toFloat()) -
-           (p2.getX().toFloat() - p3.getX().toFloat()) * (p1.getY().toFloat() - p3.getY().toFloat());
+    return (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) -
+           (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
 }
 
 bool bsp( Point const &a, Point const &b, Point const &c, Point const &point)
 {
-    float d1 = sign(point, a, b);
-    float d2 = sign(point, b, c);
-    float d3 = sign(point, c, a);
+    Fixed d1 = sign(point, a, b);
+    Fixed d2 = sign(point, b, c);
+    Fixed d3 = sign(point, c, a);
 
-    if (d1 == 0.0f || d2 == 0.0f || d3 == 0.0f)
+    if (d1 == Fixed(0) || d2 == Fixed(0) || d3 == Fixed(0))
         return false;
-    if ((d1 > 0 && d2 > 0 && d3 > 0) || (d1 < 0 && d2 < 0 && d3 < 0))
-        return true;
+    return (d1 > Fixed(0) && d2 > Fixed(0) && d3 > Fixed(0)) ||
+           (d1 < Fixed(0) && d2 < Fixed(0) && d3 < Fixed(0));
     return false;
 }
