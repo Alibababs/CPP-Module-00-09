e822ff1bbbec4b9946375a7b15e02476d20926e8
Alix Bailly <pbailly@f6r4s1.clusters.42paris.fr>
2025-07-21 14:35:10 +0200

    * AFFECT

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -2,12 +2,17 @@
 
 int main() 
 {
+    int * a = new int();
+    std::cout << "Test subject: " << *a << std::endl;
+
     try 
     {
         // Test access const
         const Array<int> const_arr(5);
         std::cout << "Access const test: " << const_arr[3] << std::endl;
+        std::cout << "Array size is: " << const_arr.size() << std::endl;
 
+        std::cout << "Exception test: ";
         const_arr[10];  // throw an exception
     } 
     catch (const std::exception& e) 
@@ -15,12 +20,19 @@ int main()
         std::cerr << "Error: " << e.what() << std::endl;
     }
 
-    // Test affectation
-    Array<int> arr(5);
-    arr[0] = 42;
-    Array<int> arr_test(3);
-    arr_test = arr;
-    std::cout << "Affectation test: " << arr_test[0] << std::endl;
+    try 
+    {
+        // Test affectation
+        Array<int> arr(5);
+        arr[0] = 42;
+        Array<int> arr_test(3);
+        arr_test = arr;
+        std::cout << "Affectation test: " << arr_test[0] << std::endl;
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 
     return 0;
 }
