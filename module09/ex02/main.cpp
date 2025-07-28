a686abb23d9508a8cee677d5c80c84a34230c45c
Alix Bailly <pbailly@f1r10s17.clusters.42paris.fr>
2025-07-28 15:31:27 +0200

    * M9

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,16 +1,15 @@
-#include "RPN.hpp"
+#include "PmergeMe.hpp"
 
 int main(int argc, char **argv) 
 {
-	if (argc != 2)
+	if (argc < 2)
 	{
-		std::cout << "Error: Need one argument" << std::endl;
-		return (1);
+		std::cerr << "Error: Not enough arguments" << std::endl;
 	}
 
-	RPN rpn;
+	PmergeMe test;
 
-	rpn.calculate(argv[1]);
+	test.sortAlgo(argc, argv);
 
 	return 0;
 }
